f40bec0e787e8e05fbfb2b315555f72ef251056c
juan jesus ojeda castelo <joc028@ual.es>
2022-01-20 20:03:18 +0100

    Agregar archivos de proyecto.

---
+++ Delegate/Covariance.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegate
+{
+    class Covariance
+    {
+        // Assignment compatibility.
+        string str = "test";
+        // An object of a more derived type is assigned to an object of a less derived type.
+        //object obj = str;
+
+        public void start()
+        {
+            // Assignment compatibility.
+            string str = "test";
+            // An object of a more derived type is assigned to an object of a less derived type.
+            object obj = str;
+
+            // Covariance.
+            IEnumerable<string> strings = new List<string>();
+            // An object that is instantiated with a more derived type argument
+            // is assigned to an object instantiated with a less derived type argument.
+            // Assignment compatibility is preserved.
+            IEnumerable<object> objects = strings;
+
+            // Contravariance.
+            // Assume that the following method is in the class:
+            // static void SetObject(object o) { }
+            Action<object> actObject = SetObject;
+            // An object that is instantiated with a less derived type argument
+            // is assigned to an object instantiated with a more derived type argument.
+            // Assignment compatibility is reversed.
+            Action<string> actString = actObject;
+        }
+
+        static void SetObject(object o) { }
+    }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Delegate
+{
+    public delegate void Del(string message);
+
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Covariance cov = new Covariance();
+            cov.start();
+
+            // Instantiate the delegate.
+            Del handler = DelegateMethod;
+
+            // Call the delegate.
+            handler("We are learning about delegate.");
+        }
+
+        // Create a method for a delegate.
+        public static void DelegateMethod(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
