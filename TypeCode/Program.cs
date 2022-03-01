c0d25eaa7b1c770d2f559fa68951103bf26923d5
juan jesus ojeda castelo <joc028@ual.es>
2022-03-01 14:50:55 +0100

    Creating the project TypeCode.

---
+++ TypeCode/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections;
+
+namespace IteratorCollection
+{
+
+
+    class Program
+    {
+        static void EscribeInfoObjeto(object obj)
+        {
+            TypeCode typeCode = Type.GetTypeCode(obj.GetType());
+            switch (typeCode)
+            {
+                case TypeCode.Boolean:
+                    Console.WriteLine("Boolean: {0}", obj);
+                    break;
+                default:
+                    Console.WriteLine("{0}:{1}", typeCode.ToString(), obj);
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            object[] objetos = { 0, 4.5, 0xAF, true };
+            foreach (var item in objetos)
+                EscribeInfoObjeto(item);
+
+
+
+        }
+
+
+    }
+}
+
