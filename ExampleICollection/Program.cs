193f85c10017e415136671581cabfe54615f255d
juan jesus ojeda castelo <joc028@ual.es>
2022-03-03 17:16:28 +0100

    Creation of the project ExampleIList.

--- ExampleICollection/Program.cs
+++ ExampleICollection/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace SwapVariables
+{
+
+
+    class Program
+    {
+        public static void Display(BoxCollection bxList)
+        {
+            Console.WriteLine("\nHeight\tLength\tWidth\tHash Code");
+            foreach (Box bx in bxList)
+            {
+                Console.WriteLine("{0}\t{1}\t{2}\t{3}",
+                    bx.Height.ToString(), bx.Length.ToString(),
+                    bx.Width.ToString(), bx.GetHashCode().ToString());
+            }
+
+            static void Main(string[] args)
+        {
+            BoxCollection bxList = new BoxCollection();
+
+            bxList.Add(new Box(10, 4, 6));
+            bxList.Add(new Box(4, 6, 10));
+            bxList.Add(new Box(6, 10, 4));
+            bxList.Add(new Box(12, 8, 10));
+
+            // Same dimensions. Cannot be added:
+            bxList.Add(new Box(10, 4, 6));
+
+            // Test the Remove method.
+            Display(bxList);
+            Console.WriteLine("Removing 6x10x4");
+            bxList.Remove(new Box(6, 10, 4));
+            Display(bxList);
+
+            // Test the Contains method.
+            Box BoxCheck = new Box(8, 12, 10);
+            Console.WriteLine("Contains {0}x{1}x{2} by dimensions: {3}",
+                BoxCheck.Height.ToString(), BoxCheck.Length.ToString(),
+                BoxCheck.Width.ToString(), bxList.Contains(BoxCheck).ToString());
+
+            // Test the Contains method overload with a specified equality comparer.
+            Console.WriteLine("Contains {0}x{1}x{2} by volume: {3}",
+                BoxCheck.Height.ToString(), BoxCheck.Length.ToString(),
+                BoxCheck.Width.ToString(), bxList.Contains(BoxCheck,
+                new BoxSameVol()).ToString());
+
+        }
+
+
+    }
+}
---
+++ ExampleIList/Program.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace ExampleIList
+{
+
+
+    class Program
+    {
+
+        
+
+        static void Main(string[] args)
+        {
+            var test = new SimpleList();
+
+            // Populate the List.
+            Console.WriteLine("Populate the List");
+            test.Add("one");
+            test.Add("two");
+            test.Add("three");
+            test.Add("four");
+            test.Add("five");
+            test.Add("six");
+            test.Add("seven");
+            test.Add("eight");
+            test.PrintContents();
+            Console.WriteLine();
+
+            // Remove elements from the list.
+            Console.WriteLine("Remove elements from the list");
+            test.Remove("six");
+            test.Remove("eight");
+            test.PrintContents();
+            Console.WriteLine();
+
+            // Add an element to the end of the list.
+            Console.WriteLine("Add an element to the end of the list");
+            test.Add("nine");
+            test.PrintContents();
+            Console.WriteLine();
+
+            // Insert an element into the middle of the list.
+            Console.WriteLine("Insert an element into the middle of the list");
+            test.Insert(4, "number");
+            test.PrintContents();
+            Console.WriteLine();
+
+            // Check for specific elements in the list.
+            Console.WriteLine("Check for specific elements in the list");
+            Console.WriteLine($"List contains \"three\": {test.Contains("three")}");
+            Console.WriteLine($"List contains \"ten\": {test.Contains("ten")}");
+
+        }
+
+
+    }
+}
