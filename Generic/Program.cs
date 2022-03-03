252406a9f0d3065cbb17af56b2b52cc18b72b8f2
juan jesus ojeda castelo <joc028@ual.es>
2022-03-03 14:42:17 +0100

    Minor changes.

--- Generic/Program.cs
+++ Generic/Program.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Diagnostics;
 using System.Globalization;
 
-namespace TemperatureIFormattable
+namespace Generic
 {
 
 
---
+++ SwapVariables/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace SwapVariables
+{
+
+
+    class Program
+    {
+
+        static void Intercambio<T>(ref T v1, ref T v2) where T : IComparable
+        {
+            if (v1.CompareTo(v2) > 0)
+            {
+                T temp = v1;
+                v1 = v2;
+                v2 = temp;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            double a = 8.2;
+            double b = 5;
+            Intercambio<double>(ref a, ref b);
+            Console.WriteLine(a + " <= " + b);
+
+        }
+
+
+    }
+}
