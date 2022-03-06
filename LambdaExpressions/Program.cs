b09e3d48020f07f62a39d40f0812c74cc886baca
juan jesus ojeda castelo <joc028@ual.es>
2022-03-06 22:59:55 +0100

    Creation of project LambdaExpressions.

---
+++ LambdaExpressions/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace AnonymousMethod
+{
+
+    class Program
+    {
+        private delegate double Mathematical(double value);
+
+        static void Main(string[] args)
+        {
+            Mathematical doublePower = (double value) => { return Math.Pow(value, 2); };
+            Mathematical halfValue = (double value) => { return value / 2; };
+
+            Console.WriteLine(doublePower(3));
+            Console.WriteLine(halfValue(3));
+        }
+
+
+    }
+}
+
+
