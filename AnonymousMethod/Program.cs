8aef881cbd607ce7b3660d1329136d018cf773a0
juan jesus ojeda castelo <joc028@ual.es>
2022-03-06 22:48:14 +0100

    Creation of the projecto AnonymousMethod.

---
+++ AnonymousMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace AnonymousMethod
+{
+
+    class Program
+    {
+        private delegate double Mathematical(double value);
+
+        static void Main(string[] args)
+        {
+            Mathematical doublePower = delegate (double value) { return Math.Pow(value, 2); };
+            Mathematical halfValue = delegate (double value) { return value / 2; };
+
+            Console.WriteLine(doublePower(3));
+            Console.WriteLine(halfValue(3));
+
+        }
+
+
+    }
+}
+
+
