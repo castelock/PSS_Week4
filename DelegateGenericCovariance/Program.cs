75ee7e218f7d544c604b7d93e48ef4d8c7ed3484
juan jesus ojeda castelo <joc028@ual.es>
2022-03-06 19:32:10 +0100

    Creation of the project DelegateGenericCovariance.

---
+++ DelegateGenericCovariance/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+namespace DelegateGenericCovariance
+{
+    public class Person { }
+    public class Employee : Person { }
+
+    class Program
+    {
+        static Employee FindByTitle(String title)
+        {
+            // This is a stub for a method that returns  
+            // an employee that has the specified title.  
+            return new Employee();
+        }
+
+        static void Main(string[] args)
+        {
+            // Create an instance of the delegate without using variance.  
+            Func<String, Employee> findEmployee = FindByTitle;
+
+            // The delegate expects a method to return Person,  
+            // but you can assign it a method that returns Employee.  
+            Func<String, Person> findPerson = FindByTitle;
+
+            // You can also assign a delegate
+            // that returns a more derived type
+            // to a delegate that returns a less derived type.  
+            findPerson = findEmployee;
+
+        }
+
+
+    }
+}
+
+
